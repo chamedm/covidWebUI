596a49e8703dc0d27fc57ea519b4b5f3b319f699
chamedm <is708177@iteso.mx>
2021-05-08 11:30:16 -0500

    Removing default code

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>To be an amazing app</p>
+    </div>
+  );
+}
+
+export default App;
