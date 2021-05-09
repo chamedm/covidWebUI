76e77a2bbae7f2c9ce6c55f2c341d6bfdf70b5c0
chamedm <is708177@iteso.mx>
2021-05-09 11:59:50 -0500

    GET all news from endpoint

--- src/app/Questions/index.js
+++ src/app/Questions/index.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import "./Questions.css";
+import { GET_NEWS_URL } from "../../assets/constants";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import SingleQuestion from "./SingleQuestion";
 
 function Questions() {
+  const [questionsList, setQuestionsList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  return(
-    <h1>Questions and answers</h1>
-  )
+  useEffect(() => {
+    fetch(GET_NEWS_URL, {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((dataJSON) => {
+        setQuestionsList(dataJSON);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="questions">
+      <div className="questions__header">
+        <h1 className="questions__title">Q&A's about COVID-19</h1>
+      </div>
+      {isLoading ? (
+        <CircularProgress color="primary" />
+      ) : (
+        <div className="questions__content">
+          {
+            questionsList.map((question, index) =>
+              {
+                return(
+                  <SingleQuestion questionObj={question} index={index} key={question.question} />
+                )
+              }
+            )
+          }
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Questions;
--- src/assets/constants.js
+++ src/assets/constants.js
@@ -4,10 +4,12 @@ const POST_NEW_REUNION_URL = "http://node-express-env.eba-yhhaeabq.us-east-1.ela
 
 const CALCULATE_INITIAL_RISK = "https://03e52a0d.us-south.apigw.appdomain.cloud/covidplatform/calculateInitialRisk";
 
+const GET_NEWS_URL = "https://nn7mj0fdmb.execute-api.us-east-1.amazonaws.com/dev/questions/";
 
 export {
   GET_ALL_REUNIONS_URL,
   POST_NEW_REUNION_URL,
   CALCULATE_INITIAL_RISK,
+  GET_NEWS_URL,
 }
 
